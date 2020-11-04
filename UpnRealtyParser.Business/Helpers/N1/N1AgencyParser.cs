120478efa730763a4439f751c865caa26456dd2d
Ivans <etherial.man@gmail.com>
2020-11-04 22:02:57 +0500

    Парсер агентств N1

---
+++ UpnRealtyParser.Business/Helpers/N1/N1AgencyParser.cs
@@ -0,0 +1,31 @@
+﻿using AngleSharp.Dom;
+using UpnRealtyParser.Business.Models;
+
+namespace UpnRealtyParser.Business.Helpers
+{
+    public class N1AgencyParser : BaseHttpParser
+    {
+        public N1Agency GetN1AgencyFromPageText(string webPageText)
+        {
+            IDocument pageHtmlDoc = getPreparedHtmlDocument(webPageText).Result;
+            return GetN1AgencyFromPageText(pageHtmlDoc, webPageText);
+        }
+
+        public N1Agency GetN1AgencyFromPageText(IDocument pageHtmlDoc, string webPageText)
+        {
+            N1Agency agency = new N1Agency();
+
+            return agency;
+        }
+
+        private void fillAgencyName(N1Agency agency, IDocument pageHtmlDoc)
+        {
+
+        }
+
+        private void fillAgentName()
+        {
+
+        }
+    }
+}
